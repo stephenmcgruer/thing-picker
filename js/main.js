14d4c58879ab46a9b39ddcb09c1697cdc576cd95
Stephen McGruer <smcgruer@chromium.org>
2017-11-01 14:15:58 -0400

    Add flickrapi, actually show photos for the given topic

--- js/main.js
+++ js/main.js
@@ -1,10 +1,58 @@
 (function(window) {
   "use strict";
 
+  // HACK(smcgruer): This is SUPER INSECURE. One should always proxy requests
+  // via a known server instead of exposing a key to js like this.
+  const FLICKR_API_KEY = "YOUR KEY HERE";
+
   const ThingPicker = {
 
+    photos: [],
+
+    current_page: 1,
+
+    flickr: new Flickr({ api_key: FLICKR_API_KEY }),
+
     showNextPhoto: function() {
-      // No-op for now.
+      // TODO(smcgruer): Grey-out the current photo and show a loading spinner.
+      if (this.photos.length == 0) {
+        this.getMorePhotos(this.showNextPhoto.bind(this));
+        return;
+      }
+
+      const photo = this.photos.shift();
+      this.showPhoto(photo);
+    },
+
+    showPhoto: function(photo) {
+      const url = "https://farm" + photo.farm + ".staticflickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + ".jpg";
+      document.getElementById("pictureBox").src = url;
+    },
+
+    getMorePhotos: function(next_photo_cb) {
+      const result_cb = function(err, result) {
+        this.handleFlickrPhotosResponse(next_photo_cb, err, result);
+      }.bind(this);
+
+      this.flickr.photos.search({
+        text: 'YOUR TOPIC HERE',
+        sort: 'relevance',
+        media: 'photos',
+        page: this.current_page,
+        per_page: '25'
+      }, result_cb);
+    },
+
+    handleFlickrPhotosResponse: function (next_photo_cb, err, result) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      for (const photo of result.photos.photo) {
+        this.photos.push(photo);
+      }
+      this.current_page += 1;
+      next_photo_cb();
     },
 
     markPhotoAs: function(description) {
